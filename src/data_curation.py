f65c4788558d4d513aae700a0e4c068310e03044
aspgln <qdai@ucla.edu>
2020-06-04 00:57:02 -0700

    create_h5 single_phase

--- src/data_curation.py
+++ src/data_curation.py
@@ -90,7 +90,7 @@ def load_json(PATH):
     with open(PATH) as f:
         js = json.load(f)
     json_data = json_normalize(js['datasets'][0]['annotations'])
-    json_data = json_data[['labelId','StudyInstanceUID','SeriesInstanceUID', 'SOPInstanceUID','data.height','data.width', 'data.x', 'data.y', 'height', 'width']]
+    json_data = json_data[['labelId','StudyInstanceUID','SeriesInstanceUID', 'SOPInstanceUID','data.height','data.width', 'data.x', 'data.y', 'height', 'width', 'annotationNumber']]
     BBId =['L_y7KqAJ', # R_arterial
     'L_e7kVd1' , # L_arterial
     'L_4lgMO1' , # R_pv
@@ -124,7 +124,7 @@ def load_redcap(PATH):
     # data_clinical = data[['accession', 'biopsy_type', 'tumor_type','pathology', 'grade']]
     # drop empty record
     data_clinical = data_clinical.dropna(subset=['mrn'])
-    data_clinical = data_clinical[['record_id', 'mrn', 'accession', 'size', 'pathology', 'grade']]
+    data_clinical = data_clinical[['record_id', 'mrn', 'accession', 'size', 'pathology', 'grade','laterality', 'stacked']]
 
     
 #     # create new df to seperate fields with 2 acc #
@@ -217,9 +217,7 @@ def show_pathology_stats(dataframe, pathology_type):
     df = df.drop_duplicates(subset=['StudyInstanceUID'])
     print()
 
-    def df_difference(df1, df2):
-        df3 = pd.concat([df1,df2]).drop_duplicates(keep=False)
-        return df3
+ 
 
     # df_multi: studies where multiple tumors on records (filtered by size)
     # df_duplicated: studies where 2 StudyInsatnceUID correponeds to 1 Patient Name
@@ -310,22 +308,12 @@ def get_fixed_bb(im, x, y, height, width, size=128):
 
 
         
-def get_cropped_img(slice_path, row):
+def crop_image(im, row):
     '''
     get the cropped bounding box
-    @slice_path: studyUID/seriesUID/sopUID
+    @im: original image
     @row: one row of df in an iteration
     '''
-    if os.path.exists(slice_path) == False:
-        print('DICOM does not exist, check UIDs.')
-        raise FileExistsError
-    else:
-        pass
-    
-    # read data pydicom
-    ds = pydicom.dcmread(slice_path)
-    im = ds.pixel_array
-    im = HU_rescale(im, ds.RescaleSlope, ds.RescaleIntercept)
 
     height = row['data.height']
     width = row['data.width']
@@ -333,35 +321,108 @@ def get_cropped_img(slice_path, row):
     y = row['data.y']
     im_crop = get_fixed_bb(im, x, y, height, width, size=128)
     return im_crop
-    
-    
-def add_group_attrs(group, df):
-    attrs_list = ['MRN', 'Pathology', 'grade', 'Tumor Type', 'Index', 'record_id']
+
+def find_max_phase(df):
+    this_series = df['labelId'].value_counts()
+    max_label = this_series.keys()[0]
+    n_max = this_series.values[0]
+    phase = check_phase(max_label)
+    if this_series.values[1] > 1:
+        print('Second common case is greater than 1!')
+        return
+    return (max_label, phase, n_max)
+
+
+def add_attrs(h5_object, df):
+
+    attrs_list = ['MRN', 'Pathology', 'grade', 'Tumor Type', 'pt_index', 'record_id']
     for attr in attrs_list:
-        group.attrs[attr] =  df[attr].drop_duplicates().squeeze()
-           
-            
-def create_h5_object(input_path, path, df_this_patient):
-    # create an hdf5 object for each patient
-    # initialize a hdf5 object
-    with h5py.File(path, mode='w', track_order=True) as f:
-        for ph in ['noncon', 'arterial', 'portal', 'delayed']:
-            group = f.create_group(ph)
-            add_group_attrs(group, df_this_patient)
-            size = 128
-            dset = group.create_dataset('data', (size,size,0), maxshape=(size,size,None), dtype='f')
+        h5_object.attrs[attr] =  df[attr].drop_duplicates().squeeze()
     
-    # add a dataset "data" to group phase. ex. arterial/data
+#     (max_label, phase, n_max) = find_max_phase(df)
+#     h5_object.attrs['annotation_phase'] = phase
+#     h5_object.attrs['annotation_label'] = max_label
+    
+    
+#     attrs_list = ['StudyInstanceUID','SeriesInstanceUID',  'SOPInstanceUID','InstanceNumber','WindowCenter', 'WindowWidth','RescaleIntercept', 'RescaleSlope','PatientID','PatientName']
+#     for attr in attrs_list:
+#         h5_object.attrs[attr] = np.empty([n_max,], dtype=object)
+    
+        
+
+def create_h5_object(input_path, path, df_this_patient, dim=128):
+    # create an hdf5 object for each patient
+    # initialization
+    with h5py.File(path, mode='w', track_order=True) as f:
+        # create single phase dset
+        group_single = f.create_group('single_phase')
+        dset = group_single.create_dataset('data', (dim,dim,0), maxshape=(dim,dim,None), dtype='f')
+        # add series level attributes
+        add_attrs(dset, df_this_patient)
+
+        # create metadata as dset
+        (max_label, phase, n_max) = find_max_phase(df_this_patient)
+        print(type(n_max))
+        metadata_list = ['StudyInstanceUID','SeriesInstanceUID',  'SOPInstanceUID','InstanceNumber','WindowCenter', 'WindowWidth','RescaleIntercept', 'RescaleSlope','PatientID','PatientName']
+        dt = h5py.string_dtype(encoding='utf-8')        
+        for x in metadata_list:
+            group_single.create_dataset(x, (int(n_max),), dtype=dt) 
+            
+
+#         dset2 = group_single.create_dataset('data', (dim,dim,0), maxshape=(dim,dim,None), dtype='f')
+
+        
+#         # create multi-phase dset
+#         group_multi = f.create_group('multi_phase')
+#         for ph in ['noncon', 'arterial', 'portal', 'delayed']:
+# #             add_group_attrs(group_multi, df_this_patient)
+#             dset = group_multi.create_dataset(ph, (dim,dim,0), maxshape=(dim,dim,None), dtype='f')
+    
     with h5py.File(path, mode='a', track_order=True) as f:
-        for index, (_, row) in enumerate(df_this_patient.iterrows()):
-            phase = check_phase(row['labelId'])
+        df_valid = df_this_patient[df_this_patient['labelId'] == max_label]
+        for index, (_, row) in enumerate(df_valid.iterrows()):
             input_slice_path = os.path.join(input_path, row['StudyInstanceUID'], 
                                    row['SeriesInstanceUID'], row['SOPInstanceUID']+'.dcm')
             
-            im_crop = get_cropped_img(input_slice_path, row)
-            data = f['{}/data'.format(phase)]
-            data.resize((size,size,data.shape[2]+1))
+            if os.path.exists(input_slice_path) == False:
+                print('DICOM does not exist, check UIDs.')
+                raise FileExistsError
+            else:
+                pass
+            
+            # read data pydicom
+            ds = pydicom.dcmread(input_slice_path)
+            im = ds.pixel_array
+            im = HU_rescale(im, ds.RescaleSlope, ds.RescaleIntercept)
+            im_crop = crop_image(im, row)
+               
+
+#             print(f['single_phase/data'].attrs)
+            data = f['single_phase/data']
+            data.resize((dim,dim,data.shape[2]+1))
             data[:,:,-1] = im_crop
+            
+
+            for x in metadata_list: 
+#                 tag= f['single_phase/InstanceNumber']
+                tag = f['single_phase/{}'.format(x)]
+                tag[index] = ds.data_element(x).value
+              
+#             f['single_phase/data'].attrs['SeriesInstanceUID'].append(ds.SeriesInstanceUID)
+#             f['single_phase/data'].attrs['SOPInstanceUID'].append(ds.SOPInstanceUID)
+#             f['single_phase/data'].attrs['InstanceNumber'].append(ds.InstanceNumber)
+#             f['single_phase/data'].attrs['WindowCenter'].append(ds.WindowCenter)
+#             f['single_phase/data'].attrs['WindowWidth'].append(ds.WindowWidth)
+#             f['single_phase/data'].attrs['RescaleIntercept'].append(ds.RescaleIntercept)
+#             f['single_phase/data'].attrs['RescaleSlope'].append(ds.RescaleSlope)
+#             f['single_phase/data'].attrs['PatientID'].append(ds.PatientID)
+#             f['single_phase/data'].attrs['PatientName'].append(ds.PatientName)
+
+#         for key, value in f['single_phase/data'].attrs.items():
+#             print(key, value)
+        print(data.shape)
+        print()
+
 
     return 
 
@@ -370,7 +431,7 @@ def create_h5_object(input_path, path, df_this_patient):
 def read_h5_object(path):
     with h5py.File(path, mode='r', track_order=True) as f:
         for p in ['noncon', 'arterial', 'portal', 'delayed']:
-            dset = f['{}/data'.format(p)]
+            dset = f['multi_phase/{}'.format(p)]
             print('\t', p, dset.shape[2])
 
             
@@ -425,7 +486,6 @@ def display_hdf5(path):
                     fig.subplots_adjust(top = 0.7, bottom=0.1)
 
 
-                    
 def drop_invalid_index(pt_index, input_path):
     '''
     Drop invalid cases
